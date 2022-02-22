588d75da6968048eb1ae8089f327440ea8b7f2f4
Z3K3GamingYT <83733589+Z3K3GamingYT@users.noreply.github.com>
2022-02-22 14:53:07 -0800

    Create lang.php

---
+++ php/lang.php
@@ -0,0 +1,15 @@
+<?php
+	$lang = array();
+
+	// admin.php
+	$lang['Next'] = 'Next';
+	$lang['Previous'] = 'Previous';
+
+	// chart.php
+	$lang['Count'] = 'Numbers';
+	$lang['Hours'] = 'Hours';
+	$lang['Days'] = 'Days';
+	$lang['New password additions in the past month'] = 'New password additions in the past month';
+	$lang['New password additions in the past 24 hours'] = 'New password additions in the past 24 hours';
+	$lang['OS popularity'] = 'OS popularity';
+	$lang['Additional OS statistics'] = 'Additional OS statistics';
