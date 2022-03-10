0455b110c9ca412de3125710542ea50c6b82f9a3
Altify <83733589+Altify-Development@users.noreply.github.com>
2022-03-10 09:36:01 -0800

    Create NewTab.js

---
+++ html/NewTab.js
@@ -0,0 +1,9 @@
+<button onclick="NewTab()">
+      Download
+  </button>
+  
+    <script>
+        function NewTab() {
+            window.open(
+              "https://github.com/Altify-Developing/altify-developing-chs", "_blank");
+}
