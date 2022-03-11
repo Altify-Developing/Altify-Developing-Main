276c900c5563cca7d089a5467154969a726b3af8
Altify <83733589+Altify-Development@users.noreply.github.com>
2022-03-11 09:36:24 -0800

    Create counter.js

---
+++ js/counter.js
@@ -0,0 +1,12 @@
+var counterContainer = document.querySelector(".website-counter");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
