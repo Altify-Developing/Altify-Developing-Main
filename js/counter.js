5a9c35c9366e5c2c20c1540a77865182165f859b
Altify <83733589+Altify-Development@users.noreply.github.com>
2022-03-11 10:03:15 -0800

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -3,10 +3,10 @@ var visitCount = localStorage.getItem("page_view");
 
 // Check if page_view entry is present
 if (visitCount) {
-  visitCount = Number(visitCount) + "1";
+  visitCount = Number(visitCount) + 1;
   localStorage.setItem("page_view", visitCount);
 } else {
-  visitCount = "1";
-  localStorage.setItem("page_view", "1");
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
 }
 counterContainer.innerHTML = visitCount;
