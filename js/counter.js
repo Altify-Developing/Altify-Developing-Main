52889659f37bc3f98256d7bb7727a1daa3b4657c
Altify <83733589+Altify-Development@users.noreply.github.com>
2022-03-14 10:12:31 -0700

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -5,11 +5,6 @@ var visitCount = localStorage.getItem("page_view");
 if (visitCount) {
   visitCount = Number(visitCount) + 1;
   localStorage.setItem("page_view", visitCount);
-  if (visitCount == 150) alert("Welcome 150th Visitor");
-  if (visitCount == 175) alert("Welcome 175th Visitor");
-  if (visitCount == 200) alert("Welcome 200th Visitor");
-  if (visitCount == 250) alert("Welcome 250th Visitor");
-  if (visitCount == 300) alert("Welcome 300th Visitor");
 } else {
   visitCount = 1;
   localStorage.setItem("page_view", 1);
