4223452b0ec53af02fe31c9cc0e89b8545129c2e
Altify <83733589+Altify-Development@users.noreply.github.com>
2022-03-14 10:10:55 -0700

    visitor count milestone-2

--- js/counter.js
+++ js/counter.js
@@ -5,8 +5,8 @@ var visitCount = localStorage.getItem("page_view");
 if (visitCount) {
   visitCount = Number(visitCount) + 1;
   localStorage.setItem("page_view", visitCount);
+  if (visitCount == 150) alert("Welcome 150th Visitor");
 } else {
   visitCount = 1;
   localStorage.setItem("page_view", 1);
 }
-if (visitCount == 150) alert("Welcome 150th Visitor");
