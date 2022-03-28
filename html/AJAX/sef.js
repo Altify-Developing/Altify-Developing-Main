05a9830706f01b60568d492b2c9897e0fdbea102
Altify <Altify@mail.com>
2022-03-28 15:27:14 -0700

    Update sef.js

--- html/AJAX/sef.js
+++ html/AJAX/sef.js
@@ -1,6 +1,8 @@
-src="obfuscatedlister.js" defer
-defer src="https://altify-developing-001.netlify.app/js/counter.js"
-  SetTimeout(Refrsh, 900);
+<script>src="obfuscatedlister.js" defer</script>
+<script>defer src="https://altify-developing-001.netlify.app/js/counter.js"</script>
+<script>  
+SetTimeout(Refrsh, 900);
     function Refrsh() {
     location.reload();
     };
+</script>
