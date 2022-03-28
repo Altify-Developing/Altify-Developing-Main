ff380e2779e1c788a26fe7b841e718e94f1c632c
Altify <Altify@mail.com>
2022-03-28 15:51:31 -0700

    Create counter.js

---
+++ html/AJAX/counter.js
@@ -0,0 +1,20 @@
+var counterContainer = document.querySelector(".website-counter");
+var milestoneContainer = document.querySelector(".milestone-counter");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+  const rndInt = Math.floor(Math.random() * 10) + 1
+  document.cookie = 'VisitorCount='+visitCount+'; expires=Wed, 1 Jan 2070 13:47:11 UTC; path=/';
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+setTimeout(Refrsh, 600);
+  function Refrsh() {
+  location.reload();
+  window.location.replace("https://altify-developing-001.netlify.app/html/ajax/info?visitor-count="+visitCount);
