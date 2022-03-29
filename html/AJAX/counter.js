21145dfc2b1e82d586277b020d636c1714fb9578
Altify <Altify@mail.com>
2022-03-28 17:51:03 -0700

    one-reload-fix

--- html/AJAX/counter.js
+++ html/AJAX/counter.js
@@ -1,5 +1,4 @@
 var counterContainer = document.querySelector(".website-counter");
-var milestoneContainer = document.querySelector(".milestone-counter");
 var visitCount = localStorage.getItem("page_view");
 // Check if page_view entry is present
 if (visitCount) {
@@ -12,7 +11,18 @@ if (visitCount) {
   localStorage.setItem("page_view", 1);
 }
 counterContainer.innerHTML = visitCount;
-function Refrsh() {
-  let visitorCount = (visitCount + 1)
+
+let reloaded = localStorage.getItem("page_view_2");
+if (reloaded) {
+  reloaded = Number(reloaded) + 1;
+  localStorage.setItem("page_view_2", reloaded);
+  const rndInt = Math.floor(Math.random() * 10) + 1
+  document.cookie = 'IsRefreshed?='+reloaded+'; path=/';
+} else {
+  reloaded = 1;
+  localStorage.setItem("page_view_2", 1);
+  let visitorCount = (reloaded + 1);
   window.location.replace("https://altify-developing-001.netlify.app/html/ajax/info?visitor-count="+visitorCount);
-};
+}
+counterContainer.innerHTML = reloaded;
+delete reloaded;
