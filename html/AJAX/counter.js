a106c94e874d0fcf42dd7f0aa2df2339652d6434
Altify <Altify@mail.com>
2022-03-28 17:16:44 -0700

    Update counter.js

--- html/AJAX/counter.js
+++ html/AJAX/counter.js
@@ -12,10 +12,3 @@ if (visitCount) {
   localStorage.setItem("page_view", 1);
 }
 counterContainer.innerHTML = visitCount;
-let btn = document.createElement("button");
-btn.innerHTML = "Refresh List";
-document.body.appendChild(btn);
-btn.onclick = function () {
-  let visitorCount = (visitCount + 1)
-  window.location.replace("https://altify-developing-001.netlify.app/html/ajax/info?visitor-count="+visitorCount);
-};
