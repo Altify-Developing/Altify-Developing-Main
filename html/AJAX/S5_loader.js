04904b59103c258606a7815eaefccca0c4a1773d
Altify <Altify@mail.com>
2022-03-29 21:14:37 -0700

    Update S5_loader.js

--- html/AJAX/S5_loader.js
+++ html/AJAX/S5_loader.js
@@ -2,5 +2,5 @@
   xhttp.onload = function() {
     document.getElementById("S5-List").innerHTML = this.responseText;
     }
-  xhttp.open("GET", "SOCKS5.js", true);
+  xhttp.open("GET", "SOCKS5.html", true);
   xhttp.send();
