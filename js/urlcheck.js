1bca6a75419e6004ee81addd1ba9c050a9cce1da
Altify <Altify@mail.com>
2022-04-01 10:55:56 -0700

    Create urlcheck.js

---
+++ js/urlcheck.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    if (window.location.href.indexOf("csrf=true") > -1) {
+      setInterval(randomString, 1000);
+        setInterval(refrsh, 1000);
+        function refrsh() {
+          window.location.replace("https://altify-developing-001.netlify.app/html/oag/aio_plain.src.html/?circulation=true&ref%20count="+refcount);
+        }
+    }
+  });
