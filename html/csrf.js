78d1cc8cdd30d86f718f6e0ce487c7532cb84c08
Altify <Altify@mail.com>
2022-03-31 17:45:18 -0700

    Update csrf.js

--- html/csrf.js
+++ html/csrf.js
@@ -15,4 +15,4 @@ document.getElementById("csrfid2").innerHTML = (csrf2);
 "<p id='csrfid'></p>"
 );*/
 alert(csrf3);
-document.getElementById("csrfid4").innerHTML = ('<p>x-csrf token='+csrf3);
+document.getElementById("csrfid4").innerHTML = ("<label for=csrfid'>:</label><input type='text' id='csrfid' name='csrfid' value="+csrf3+"><br>");
