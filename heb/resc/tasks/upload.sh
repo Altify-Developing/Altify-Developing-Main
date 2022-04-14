1c550b0f44735a0ae15c84431c0ff9d1dc959d79
Altify <Altify@mail.com>
2022-04-13 20:55:43 -0700

    Update upload.sh

--- heb/resc/tasks/upload.sh
+++ heb/resc/tasks/upload.sh
@@ -13,8 +13,8 @@ rand=$[$RANDOM % ${#arr[@]}]
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 d2=`date -d '1 days ago' +'%B'`
 d3=`date -d '1 days ago' +'%d'`
-git --no-pager --since='1 days ago' log
-git --no-pager --since='1 days ago' log > ./heb/resc/tasks/logs/log.md
+git --no-pager --since="1 days ago" log
+git --no-pager --since="1 days ago" log > ./heb/resc/tasks/logs/log.md
 git config --local user.email "Altify@mail.com"
 git config --local user.name "Altify-Development"
 git commit -am "${arr[$rand]} (at ${d})"
