1016d7917b83390521c8f4b849703d19e816421c
Altify <Altify@mail.com>
2022-04-13 22:00:46 -0700

    Update upload.sh

--- heb/resc/tasks/upload.sh
+++ heb/resc/tasks/upload.sh
@@ -11,10 +11,10 @@ arr[7]="bot: Altify Update - requisition | Tasks Running | Funny Message: me wri
 
 rand=$[$RANDOM % ${#arr[@]}]
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
-git log --name-status HEAD^..HEAD
-git log -95 > ./heb/resc/tasks/logs/log.md
 git config --local user.email "Altify@mail.com"
 git config --local user.name "Altify"
-git commit -am "${arr[$rand]} (at ${d})"
+git log --name-status HEAD^..HEAD
+git log -95 > ./heb/resc/tasks/logs/log.md
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Altify-Developing/Altify-Developing-Main/traffic/clones > ./heb/resc/tasks/logs/traffic.md
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/OWNER/REPO/stats/code_frequency 
+git commit -am "${arr[$rand]} (at ${d})"
