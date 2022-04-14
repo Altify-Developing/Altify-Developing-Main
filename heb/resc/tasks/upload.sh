b6fbb1f93951f135d59484cb51e9beced182bb55
Altify <Altify@mail.com>
2022-04-13 21:02:58 -0700

    Update upload.sh

--- heb/resc/tasks/upload.sh
+++ heb/resc/tasks/upload.sh
@@ -11,10 +11,7 @@ arr[7]="bot: Altify Update - requisition | Tasks Running | Funny Message: me wri
 
 rand=$[$RANDOM % ${#arr[@]}]
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
-d2=`date -d '1 days ago' +'%B'`
-d3=`date -d '1 days ago' +'%d'`
-git log --name-status HEAD^..HEAD
-git log -1 > ./heb/resc/tasks/logs/log.md
+git log --name-status HEAD^..HEAD > ./heb/resc/tasks/logs/log.md
 git config --local user.email "Altify@mail.com"
 git config --local user.name "Altify-Development"
 git commit -am "${arr[$rand]} (at ${d})"
