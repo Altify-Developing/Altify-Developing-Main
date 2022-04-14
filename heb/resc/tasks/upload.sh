5b1e27f42293fc407aca7942f7a2414514198188
Altify <Altify@mail.com>
2022-04-13 20:16:30 -0700

    Update upload.sh

--- heb/resc/tasks/upload.sh
+++ heb/resc/tasks/upload.sh
@@ -12,7 +12,7 @@ arr[7]="bot: Altify Update - requisition | Tasks Running | Funny Message: me wri
 rand=$[$RANDOM % ${#arr[@]}]
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 d2=`date -d '1 days ago' +'%B %d %Y'`
-echo "git --no-pager log --since='${d2}' --until='${d2}'"> ./heb/resc/tasks/logs/log.md
+git --no-pager log --since='${d2}' --until='${d2}' > ./heb/resc/tasks/logs/log.md
 git config --local user.email "Altify@mail.com"
 git config --local user.name "Altify-Development"
 git commit -am "${arr[$rand]} (at ${d})"
