e53a8fb85e691c4695472a8fb8eaab088b97b39a
Altify <Altify@mail.com>
2022-04-15 11:12:32 -0700

    xdg help

--- heb/resc/tasks/Browsing.sh
+++ heb/resc/tasks/Browsing.sh
@@ -49,7 +49,7 @@ joke=$[$RANDOM % ${#ayb[@]}]
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 git config --local user.email "Altify@mail.com"
 git config --local user.name "Altify"
-xdg-open 'https://github.com/Altify-Developing/Altify-Developing-Main'
+xdg-open --help > ./heb/resc/tasks/browser/info/xdg.md
 git --version > ./heb/resc/tasks/browser/info/version.md
 git status > ./heb/resc/tasks/browser/info/status.md
 git commit -a -m "${arr[$rand]} (at ${d})" -m "Tasks:
