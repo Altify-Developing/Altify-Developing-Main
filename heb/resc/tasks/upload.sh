535ddf727ca40c6c946e9041fd8bb12cdd9ae1d2
Altify <Altify@mail.com>
2022-04-15 09:02:00 -0700

    Update upload.sh

--- heb/resc/tasks/upload.sh
+++ heb/resc/tasks/upload.sh
@@ -15,6 +15,6 @@ git config --local user.email "Altify@mail.com"
 git config --local user.name "Altify"
 git log --name-status HEAD^..HEAD
 git log -95 > ./heb/resc/tasks/logs/log.md
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Altify-Developing/Altify-Developing-Main/traffic/clones > ./heb/resc/tasks/logs/traffic.md
+curl -H "Accept: application/vnd.github.v3+json owner: ${{ github.actor }}" https://api.github.com/repos/Altify-Developing/Altify-Developing-Main/traffic/clones > ./heb/resc/tasks/logs/traffic.md
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/OWNER/REPO/stats/code_frequency 
 git commit -am "${arr[$rand]} (at ${d})"
