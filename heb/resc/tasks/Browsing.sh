5a3a88f918129972c7104526ec87a51891831883
Altify <Altify@mail.com>
2022-04-15 12:42:21 -0700

    Update Browsing.sh

--- heb/resc/tasks/Browsing.sh
+++ heb/resc/tasks/Browsing.sh
@@ -49,7 +49,8 @@ joke=$[$RANDOM % ${#ayb[@]}]
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 git config --local user.email "Altify@mail.com"
 git config --local user.name "Altify"
-git web--browse -b google-chrome "https://github.com/Altify-Developing/Altify-Developing-Main"
+git config --global web.browser chromium
+git web--browse "https://github.com/Altify-Developing/Altify-Developing-Main"
 xdg-open --help > ./heb/resc/tasks/browser/info/xdg.md
 git --version > ./heb/resc/tasks/browser/info/version.md
 git status > ./heb/resc/tasks/browser/info/status.md
