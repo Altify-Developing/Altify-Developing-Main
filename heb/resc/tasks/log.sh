e6a7eb237dff8d7c2f77de9025247d8bb8209427
Altify <Altify@mail.com>
2022-04-15 17:48:44 -0700

    Update log.sh

--- heb/resc/tasks/log.sh
+++ heb/resc/tasks/log.sh
@@ -13,6 +13,7 @@ rand=$[$RANDOM % ${#arr[@]}]
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 git config --local user.email "Altify@mail.com"
 git config --local user.name "Altify"
+git config commit.gpgsign true
 git log -95 > ./heb/resc/tasks/logs/log.md
 git --version > ./heb/resc/tasks/logs/git/version.md
 git status > ./heb/resc/tasks/logs/git/status.md
