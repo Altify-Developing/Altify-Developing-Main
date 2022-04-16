202f607bb7fd229729292912ee0b1e71260b4b9c
Altify <Altify@mail.com>
2022-04-15 17:52:36 -0700

    gpg verify

--- heb/resc/tasks/log.sh
+++ heb/resc/tasks/log.sh
@@ -13,11 +13,12 @@ rand=$[$RANDOM % ${#arr[@]}]
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 git config --local user.email "Altify@mail.com"
 git config --local user.name "Altify"
+git config --global user.signingKey 4AEE18F83AFDEB23
 git config commit.gpgsign true
 git log -95 > ./heb/resc/tasks/logs/log.md
 git --version > ./heb/resc/tasks/logs/git/version.md
 git status > ./heb/resc/tasks/logs/git/status.md
-git commit -a -m "${arr[$rand]} (at ${d})" -m "Logging Information:
+git commit -a -S -m "${arr[$rand]} (at ${d})" -m "Logging Information:
 - Status: https://github.com/Altify-Developing/Altify-Developing-Main/blob/main/heb/resc/tasks/logs/git/status.md
 - Version: https://github.com/Altify-Developing/Altify-Developing-Main/blob/main/heb/resc/tasks/logs/git/version.md
 - Timestamp: ${d}"
