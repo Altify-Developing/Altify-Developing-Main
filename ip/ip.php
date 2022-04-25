ea92d9b2b739bf60bd9d83db639ce45ba69275aa
Altify <Altify@mail.com>
2022-04-25 14:30:16 -0700

    Update ip.php

--- ip/ip.php
+++ ip/ip.php
@@ -1,6 +1,6 @@
 <?php
 $file = fopen("./ip.txt","a");
-$ip_address1 = $_SERVER['HTTP_CLIENT_IP'];
+$ip_address1 = "$_SERVER['HTTP_CLIENT_IP'] blah";
 $ip_address2 = $_SERVER['HTTP_X_FORWARDED_FOR'];
 $ip_address3 = $_SERVER['REMOTE_ADDR'];
 echo $ip_address1;
