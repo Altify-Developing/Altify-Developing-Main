7e2156466e54a20c2c939eacd128d16922d64ea0
Altify <Altify@mail.com>
2022-04-25 12:54:51 -0700

    Update ip.php

--- ip/ip.php
+++ ip/ip.php
@@ -1,6 +1,18 @@
 <?php
 $file = fopen("./ip.txt","a");
-$ip=$_SERVER['REMOTE_ADDR'];
-echo fwrite($file,$ip);
+if (!empty($_SERVER['HTTP_CLIENT_IP']))   
+  {
+    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+  }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
+  {
+    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  }
+else
+  {
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+  }
+echo $ip_address;
+echo fwrite($file,$ip_address);
 fclose($file);
 ?>
