b3b574945b95cfe82a533f52e4ee3d895ff463a5
Altify <Altify@mail.com>
2022-04-28 13:41:36 -0700

    Update urlcheck.js

--- js/urlcheck.js
+++ js/urlcheck.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
-    if (window.location.href.indexOf("csrf=true") > -1) {
-      setInterval(randomString, 1000);
-        setInterval(refrsh, 1000);
-        function refrsh() {
-          window.location.replace("https://altify-developing-001.netlify.app/html/oag/aio_plain.src.html/?circulation=true&ref%20count="+refcount);
-        }
-    }
-  });
+  if (window.location.href.indexOf("destination=tos") > -1) {
+    window.location.replace("https://altify-developing-001.netlify.app/html/ToS.html");
+  }
+  if (window.location.href.indexOf("destination=github") > -1) {
+    window.location.replace("https://github.com/Altify-Developing/Altify-Developing-Main");
+  }
+  if (window.location.href.indexOf("destination=tools") > -1) {
+    window.location.replace("https://altify-developing-001.netlify.app/html/toolstodownload");
+  }
+  if (window.location.href.indexOf("destination=current") > -1) {
+    window.location.replace("https://altify-developing-001.netlify.app/currentcount");
+  }
+  if (window.location.href.indexOf("destination=keylogger2") > -1) {
+    window.location.replace("https://ip-tracing.netlify.app/");
+  }
+  if (window.location.href.indexOf("destination=keylogger") > -1) {
+    window.location.replace("https://altify-developing-001.netlify.app/html/ResponsiveKeyLogger/info");
+  }
+});
