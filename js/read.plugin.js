4ed1e018f008b9612e214ac058e8d675efad5ede
Altify <Altify@mail.com>
2022-08-29 14:58:39 -0700

    Create read.plugin.js

---
+++ js/read.plugin.js
@@ -0,0 +1,6 @@
+function replace() {
+  document.getElementById("goto").innerHTML = new;
+  var origin = window.location.href
+  let origal = location.origin;
+  window.location.replace(origal+"/?"+new);
+}
