4cb66a99ed0a0a1d61e6aaa5cad107c9eeec8c24
Altify <Altify@mail.com>
2022-08-30 22:34:11 -0700

    Update urlcheck.js

--- js/urlcheck.js
+++ js/urlcheck.js
@@ -88,3 +88,93 @@ $(document).ready(function() {
     window.location.replace("https://github.com/Altify-Developing/IP-TRACE-INDEPTH");
   }
 });
+function reader() {
+  let origin = location.origin;
+  if (window.location.href.indexOf("current") > -1) {
+    window.location.replace(origin+"/currentcount");
+  }
+  if (window.location.href.indexOf("keylogger2") > -1) {
+    window.location.replace("https://ip-tracing.netlify.app/");
+  }
+  if (window.location.href.indexOf("keylogger") > -1) {
+    window.location.replace(origin+"/html/ResponsiveKeyLogger/info");
+  }
+  if (window.location.href.indexOf("calculator") > -1) {
+    window.location.replace(origin+"/html/new_tools/trig_calc/calculator");
+  }
+  if (window.location.href.indexOf(("proxy") || ("pg")) > -1) {
+    window.location.replace(origin+"/html/NEW_TOOLS/ProxyGen");
+  }
+  if (window.location.href.indexOf(("new") || ("latest")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/Fake-Person-Generator");
+  }
+  if (window.location.href.indexOf(("fakeperson") || ("fakegen")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/Fake-Person-Generator");
+  }
+  if (window.location.href.indexOf(("person") || ("fpg")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/Fake-Person-Generator");
+  }
+  if (window.location.href.indexOf(("combo") || ("cleaner")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/combocleaner-custom");
+  }
+  if (window.location.href.indexOf(("list") || ("altifycleaner")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/combocleaner-custom");
+  }
+  if (window.location.href.indexOf(("gmail") || ("generator")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/Gmail-Generator-Python");
+  }
+  if (window.location.href.indexOf("pythongmail") > -1) {
+    window.location.replace("https://github.com/Altify-Development/Gmail-Generator-Python");
+  }
+  if (window.location.href.indexOf(("discord") || ("join")) > -1) {
+    window.location.replace("https://joindiscord.netlify.app/");
+  }
+  if (window.location.href.indexOf(("help") || ("faq")) > -1) {
+    window.location.replace("https://joindiscord.netlify.app/");
+  }
+  if (window.location.href.indexOf(("input") || ("blocker")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/Keyboard-and-mouse-Input-Blocker");
+  }
+  if (window.location.href.indexOf(("keyblock") || ("keystopper")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/Keyboard-and-mouse-Input-Blocker");
+  }
+  if (window.location.href.indexOf(("spotify") || ("spoty")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/Premium-Spotify-Account-Generator-Python-2022");
+  }
+  if (window.location.href.indexOf("krnl") > -1) {
+    window.location.replace("https://github.com/Altify-Development/krnl-linkvertise-bypass");
+  }
+  if (window.location.href.indexOf(("backdoor") || ("rat")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/windows-backdoor");
+  }
+  if (window.location.href.indexOf("val") > -1) {
+    window.location.replace("https://github.com/Altify-Development/Valorant-Account-Generator-Python-2022");
+  }
+  if (window.location.href.indexOf("nike") > -1) {
+    window.location.replace("https://github.com/Altify-Development/Nike-Account-Generator-Python-2022");
+  }
+  if (window.location.href.indexOf(("steam") || ("sac")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/Steam-Account-Generator-Python-2022");
+  }
+  if (window.location.href.indexOf("google") > -1) {
+    window.location.replace("https://github.com/Altify-Development/Google-Account-Generator-Python");
+  }
+  if (window.location.href.indexOf(("anticlose") || ("")) > -1) {
+    window.location.replace("https://github.com/Altify-Development/Anti-Close-Keylogger-AutoHotKey");
+  }
+  if (window.location.href.indexOf("main") > -1) {
+    window.location.replace("https://github.com/Altify-Developing/Altify-Developing-Main");
+  }
+  if (window.location.href.indexOf(("youtube") || ("money")) > -1) {
+    window.location.replace("https://youtubemoney.netlify.app/");
+  }
+  if (window.location.href.indexOf(("converter") || ("calculator")) > -1) {
+    window.location.replace("https://youtubemoney.netlify.app/");
+  }
+  if (window.location.href.indexOf(("flooder") || ("kahoot")) > -1) {
+    window.location.replace("https://github.com/Altify-Developing/kahoot-flooder");
+  }
+  if (window.location.href.indexOf(("ip") || ("trace")) > -1) {
+    window.location.replace("https://github.com/Altify-Developing/IP-TRACE-INDEPTH");
+  }
+}
