10ff00f349181774dd75d05cf8d6cadd894beea2
Altify <Altify@mail.com>
2022-08-30 22:41:49 -0700

    Delete ip directory

--- ip/ip.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = fopen("./ip.txt","a");
-$ip_address1 = "$_SERVER['HTTP_CLIENT_IP'] blah";
-$ip_address2 = $_SERVER['HTTP_X_FORWARDED_FOR'];
-$ip_address3 = $_SERVER['REMOTE_ADDR'];
-echo $ip_address1;
-echo fwrite($file,$ip_address1);
-fclose($file);
-?>
