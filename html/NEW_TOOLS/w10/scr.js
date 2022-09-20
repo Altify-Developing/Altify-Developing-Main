b174810b0d9f4dbc6b86e358900be41b11ce8ea9
Altify <Altify@mail.com>
2022-09-20 11:49:07 -0700

    Create scr.js

---
+++ html/NEW_TOOLS/w10/scr.js
@@ -0,0 +1,82 @@
+function copyElementText(id) {
+    var text = document.getElementById(id).innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+}
+  function randomString(length, chars) {
+    refcount = Math.floor(Math.random() * 100000) + 1;
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+  }
+function setvar() {
+        wa1 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wa2 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wa3 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wa4 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let wain = (wa1+'-'+wa2+'-'+wa3+'-'+wa4);
+        wb1 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wb2 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wb3 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wb4 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let wbin = (wb1+'-'+wb2+'-'+wb3+'-'+wb4);
+        wc1 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wc2 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wc3 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wc4 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let wcin = (wc1+'-'+wc2+'-'+wc3+'-'+wc4);
+        wd1 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wd2 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wd3 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wd4 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let wdin = (wd1+'-'+wd2+'-'+wd3+'-'+wd4);
+        we1 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        we2 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        we3 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        we4 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let wein = (we1+'-'+we2+'-'+we3+'-'+we4);
+        wf1 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wf2 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wf3 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wf4 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let wfin = (wf1+'-'+wf2+'-'+wf3+'-'+wf4);
+        wg1 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wg2 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wg3 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wg4 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let wgin = (wg1+'-'+wg2+'-'+wg3+'-'+wg4);
+        wh1 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wh2 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wh3 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wh4 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let whin = (wh1+'-'+wh2+'-'+wh3+'-'+wh4);
+        wi1 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wi2 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wi3 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wi4 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let wiin = (wi1+'-'+wi2+'-'+wi3+'-'+wi4);
+        wj1 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wj2 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wj3 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        wj4 = randomString(5, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let wjin = (wj1+'-'+wj2+'-'+wj3+'-'+wj4);
+  
+  			let fnad = ("Socks4 Proxies Provided By Altify#0001 - discord.gg/alts");
+  
+  const advert = [fnad];
+  const proxies = [wain, wbin, wcin, wdin, wein, wfin, wgin, wfin, wgin, whin, whin, wiin, wjin];
+  
+  let txt1 = "";
+  proxies.forEach(Proxies);
+  document.getElementById("wintotal").innerHTML = txt1;
+  function Proxies(value, index, array) {
+    txt1 += value + "<br>"; 
+  };
+  function Advert(value, index, array) {
+    txt2 += value + "<br>"; 
+  };
+  };
