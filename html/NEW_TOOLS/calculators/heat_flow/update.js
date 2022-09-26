e09c5fc3e6f73e3a6cca90d204a2f1d53ccd9a5f
Altify <Altify@mail.com>
2022-09-26 11:35:04 -0700

    Update update.js

--- html/NEW_TOOLS/calculators/heat_flow/update.js
+++ html/NEW_TOOLS/calculators/heat_flow/update.js
@@ -1,3 +1,3 @@
 document.getElementById("showmore").addEventListener("click", function() {
-  document.getElementById("showmore1").innerHTML = ("Mass is measured in grams, Specific heat and change in temperature is measured as Celsius, and change in tempearture should be calculated as initial avg - final avg");
+  document.getElementById("showmore1").id = ("Mass is measured in grams, Specific heat and change in temperature is measured as Celsius, and change in tempearture should be calculated as initial avg - final avg");
 }
