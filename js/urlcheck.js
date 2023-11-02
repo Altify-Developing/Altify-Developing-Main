03708bd88e7ca553c2825a65cc0d438d5c468cdc
Altify <Altify@mail.com>
2023-11-02 12:00:13 -0700

    Update urlcheck.js
    fix of url not being checked for certain words
    
    Signed-off-by: Altify <Altify@mail.com>

--- js/urlcheck.js
+++ js/urlcheck.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   if (window.location.href.indexOf("calculator") > -1) {
     window.location.replace(origin+"/html/new_tools/trig_calc/calculator");
   }
-  if (window.location.href.indexOf(("oldproxy") || ("oldgen")) > -1) {
+  if (window.location.href.indexOf("oldproxy" || "oldgen") > -1) {
     window.location.replace(origin+"/html/NEW_TOOLS/ProxyGen");
   }
   if (window.location.href.indexOf(("new") || ("latest")) > -1) {
