4380009de27e94ad958bf5ff225ceb4d4024273c
Altify <Altify@mail.com>
2024-12-06 09:27:12 -0800

    Create update.js
    Signed-off-by: Altify <Altify@mail.com>

---
+++ html/NEW_TOOLS/calculators/money/update.js
@@ -0,0 +1,4 @@
+document.getElementById("showmore").addEventListener("click", function() {
+  document.getElementById("showmore1").innerHTML = "The money will be calculated as is, coins added after main dollars are added as their proper values do not use commas as it will break the code";
+  
+})
